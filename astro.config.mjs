51881eda78261eb6a12c95d3a23e8513e392f26c
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2023-09-12 14:50:14 +0200

    fixed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mariehaahr.github.io',
-	base: '/astro',
+	base: '/notes',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		remarkPlugins: [
